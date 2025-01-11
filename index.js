c820ddcf07b9a1debb049c2260281d29823b0704
Mohamed-Trachi <114502538+Mohamed-Trachi@users.noreply.github.com>
2025-01-11 15:55:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
 require('dotenv').config()
 
 var app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 
 const port = process.env.PORT || 3000;
